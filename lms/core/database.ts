4f8856d58bc3f964e3d122a5b8a463f2629590b7
niusdev <vinidamascenadev@gmail.com>
2026-07-21 14:50:59 -0300

    feat: adicionar classe LmsApi e AuthApi, criando as tabelas do sistema

--- lms/core/database.ts
+++ lms/core/database.ts
@@ -8,7 +8,7 @@ export class Database extends DatabaseSync {
       PRAGMA foreign_keys = 1;
       PRAGMA journal_mode = DELETE;
       PRAGMA synchronous = NORMAL;
-      
+
       PRAGMA cache_size = 2000;
       PRAGMA busy_timeout = 5000;
       PRAGMA temp_store = MEMORY;
--- lms/index.ts
+++ lms/index.ts
@@ -1,11 +1,13 @@
-import { ProductsApi } from "./api/products/index.ts";
+import { AuthApi } from "./api/auth/index.ts";
+import { LmsApi } from "./api/lms/index.ts";
 import { Core } from "./core/core.ts";
 import { logger } from "./core/middlewares/logger.ts";
 
 const core = new Core();
 core.router.use([logger]);
 
-new ProductsApi(core).init();
+new AuthApi(core).init();
+new LmsApi(core).init();
 
 core.router.get("/", (req, res) => {
   res.status(200).json("Hello!");
