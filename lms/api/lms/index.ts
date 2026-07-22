8d987f15aa45d0b0cced629c5f2102e03c06e906
niusdev <vinidamascenadev@gmail.com>
2026-07-22 16:19:07 -0300

    feat: adicionar aulas e cursos no banco

--- lms/api/lms/index.ts
+++ lms/api/lms/index.ts
@@ -23,6 +23,35 @@ export class LmsApi extends Api {
         title: "curso criado",
       });
     },
+    postLesson: (req, res) => {
+      const {
+        courseSlug,
+        slug,
+        title,
+        seconds,
+        video,
+        description,
+        order,
+        free,
+      } = req.body;
+
+      const writeResult = this.db
+        .query(
+          /*sql*/ `
+          INSERT OR IGNORE INTO "lessons"
+          ("course_id", "slug", "title", "seconds", "video", "description","order" , "free")
+          VALUES ((SELECT "id" FROM "courses" WHERE "slug" = ?),?,?,?,?,?,?,?)`,
+        )
+        .run(courseSlug, slug, title, seconds, video, description, order, free);
+      if (writeResult.changes === 0) {
+        throw new RouteError(400, "erro ao criar aula");
+      }
+      res.status(201).json({
+        id: writeResult.lastInsertRowid,
+        changes: writeResult.changes,
+        title: "aula criada",
+      });
+    },
   } satisfies Api["handlers"];
 
   tables(): void {
@@ -31,5 +60,6 @@ export class LmsApi extends Api {
 
   routes(): void {
     this.router.post("/lms/courses", this.handlers.postCourses);
+    this.router.post("/lms/lessons", this.handlers.postLesson);
   }
 }
